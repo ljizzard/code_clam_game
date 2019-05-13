64d7b4a0548bef6bb0cc7de4bdd8264720f62cb1
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-05-13 13:21:33 +0100

    code change

--- client/src/components/CardGrid.vue
+++ client/src/components/CardGrid.vue
@@ -26,66 +26,98 @@ export default {
       level: 1
     }
   },
-  computed: {
-    matchesLeft() {
-      let matchedRemaining = 0
-      let cardsLength = this.cards.length
-
-      for(let i = 0; i < cardsLength; i++) {
-        if (this.cards[i].matched == true) {
-          ++matchedRemaining
-        }
-      }
-      return ((cardsLength - matchedRemaining)/2)
-    }
-  },
   methods: {
-    handleClickEventFromChild(card) {
+    const gameGrid = creatures
+     .concat(creatures)
+     .sort (() => 0.5 - Math.random());
 
-      card.showFace = !card.showFace
-      ++this.clickCount
-      this.guesses.push(card)
+    let firstGuess = '';
+    let secondGuess = '';
+    let count = 0;
+    let previousTarget = null;
+    let delay = 1200;
 
-      if(this.guesses.length > 1) {
-        this.compareGuesses()
-      }
-    },
+    const game = document.getElementById('game');
+    const grid = document.createElement('section');
+    grid.setAttribute('class', 'grid');
+    game.appendChild(grid);
 
-    compareGuesses() {
-      const self = this
+    gameGrid.forEach(item => {
+     const { name, img } = item;
 
-      if(this.guesses[0].face.name === this.guesses[1].face.name) {
-        this.guesses[1].matched = true
-        this.guesses[0].matched = true
-        this.resetRound()
-      } else {
-        setTimeout(function() {
-          self.resetRound()
-        }, 500)
-      }
-    },
+     const card = document.createElement('div');
+     card.classList.add('card');
+     card.dataset.name = name;
 
-    resertRound() {
-      this.clickCount = 0
-      this.guesses = []
-      ++this.level
-      for(let i = 0; i < this.cards.length; i++){
-        let card = this.cards[i]
+     const front = document.createElement('div');
+     front.classList.add('front');
 
-        if(card.showFace === true && card.matched === false){
-          this.cards[i].showFace = false
-        }
-      }
-    },
+     const back = document.createElement('div');
+     back.classList.add('back');
+     back.style.backgroundImage = `url(${img})`;
 
-    resetGame() {
-      this.clickCount = 0
-      this.matchingOptions.forEach(function(element){
-        element.pairs = 2;
-      });
-      this.level = 1
-      this.guesses = []
-      this.cards = []
+     grid.appendChild(card);
+     card.appendChild(front);
+     card.appendChild(back);
+    });
+
+    const match = () => {
+     const selected =
+     document.querySelectorAll('.selected');
+     selected.forEach(card => {
+       card.classList.add('match');
+     });
+    };
+
+    const resetGuesses = () => {
+     firstGuess = '';
+     secondGuess = '';
+     count = 0;
+     previousTarget = null;
+
+     var selected =
+     document.querySelectorAll('.selected');
+     selected.forEach(card => {
+       card.classList.remove('selected');
+     });
+    };
+
+    grid.addEventListener('click', event => {
+
+     const clicked = event.target;
+
+     if (
+       clicked.nodeName === 'SECTION' ||
+       clicked === previousTarget ||
+       clicked.parentNode.classList.contains('selected') ||
+       clicked.parentNode.classList.contains('match')
+     ) {
+       return;
+     }
+     if ( count < 2 ) {
+       count ++;
+       if (count === 1) {
+         firstGuess =
+         clicked.parentNode.dataset.name;
+         console.log(firstGuess);
+         clicked.parentNode.classList.add('selected');
+       } else {
+         secondGuess =
+         clicked.parentNode.dataset.name;
+         console.log(secondGuess);
+         clicked.parentNode.classList.add('selected');
+       }
+
+       if (firstGuess && secondGuess) {
+         if (firstGuess === secondGuess) {
+           setTimeout(match, delay);
+         }
+         setTimeout(resetGuesses, delay);
+       }
+       previousTarget = clicked;
+     }
+
+    })
     }
   }
 }
