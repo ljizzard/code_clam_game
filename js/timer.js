726356227f090933e0f7532c6a739c7318f3df9c
lindsey <lj0978@hotmail.co.uk>
2019-05-15 10:48:52 +0100

    audio css fail

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,1 @@
 
-<div id="timer"> 0s </div>
-
-
-function timer(){
-setTimeout(function(){
-  var timerDiv = document.getElementById("timer");
-  time++;
-  timerDiv.InnerHTML = time;
-  timer();
-}, 1000)
-}
